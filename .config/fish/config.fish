6338fb520a69ff2c645d40ffd31026a2c7b1acd1
Bento Carlos <bcsilva49@gmail.com>
2021-01-10 11:11:51 -0300

    Added Fish shell config

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+# Command line alias to start the browser-sync server
+alias serve="browser-sync start --server --files ."
+
+# Aliases 
+# Launch FISH config file
+alias ef="nvim $HOME/.config/fish/config.fish"
+# Launch ZSH config file
+alias ec="nvim $HOME/.zshrc"
+
+# source ~/.zshrc
+alias sc="source $HOME/.zshrc"
+
+# Launch Nvim config file
+alias en="nvim $HOME/.config/nvim/init.vim"
+
+# Replace ls with exa
+alias l='exa'
+alias ll='exa -l'
+alias ls='exa -lah'
